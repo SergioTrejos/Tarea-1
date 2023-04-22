b59524d322f5d3ebaf0814be9482cbbabe433be1
Sergio Trejos <sergio.trejosquiros@ucr.ac.cr>
2023-04-22 16:42:35 -0600

    Añadiendo los constructores

--- Tarea1.cpp
+++ Tarea1.cpp
@@ -9,18 +9,32 @@ class Aula{
         int _identificador;
         int _pupitres;
         bool _proyector;
+    Aula(int piso, int identificador, int pupitres, bool proyector){
+        _piso = piso;
+        _identificador = identificador;
+        _pupitres = pupitres;
+        _proyector = proyector;
+    };
 };
 
 class Edificio{
     public:
         bool _ascensor;
         string _nombre;
+    Edificio(bool ascensor, string nombre){
+        _ascensor = ascensor;
+        _nombre = nombre;
+    }
 };
 
 class Edicifio_aulas:public Edificio{
     public:
         bool _soda;
-        Aula _aulas[];
+        Aula* _aulas;
+    Edicifio_aulas(bool soda, Aula* aulas, bool ascensor, string nombre):Edificio(ascensor, nombre){
+        _soda = soda;
+        this -> _aulas = aulas;
+    };
 };
 
 class Edificio_parqueo:public Edificio{
@@ -31,13 +45,22 @@ class Finca{
     public:
         string _nombre;
         bool _bus;
-        Edificio _edificios[];
+        Edificio* _edificios;
+    Finca(string nombre, bool bus, Edificio* edificios, bool ascensor){
+        _nombre = nombre;
+        _bus = bus;
+        this -> _edificios = edificios;
+    };
 };
 
 class Campus{
     public:
         string _nombre;
-        Finca _fincas[];
+        Finca* _fincas;
+    Campus(string nombre, Finca* fincas){
+        _nombre = nombre;
+        this -> _fincas = fincas;
+    }
 };
 
 int main(void){
