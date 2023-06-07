e63214c5b08b0233d7f4d7ccc354295e45cc5025
Sergio Trejos <sergio.trejosquiros@ucr.ac.cr>
2023-06-07 16:20:07 -0600

    update del programa

--- tarea4/empresa.py
+++ tarea4/empresa.py
@@ -2,8 +2,31 @@ archivo = open("c:/Users/Sergio/Documents/2k23/Estructuras/Tarea3/datos_empresa.
 lineas = archivo.readlines()
 archivo.close()
 
-def validar_entrada():
+def validar_entrada(entrada):
+    listas = entrada.split(",")
+    if len(listas) == 5 and isinstance (str,listas[0]) and isinstance (str,listas[1]) and isinstance (str,listas[3]):
+        try:
+            int(listas[2])
+            int(listas[4])
+            return True
+        except:
+            return False
+    else:
+        return False
     
+def leer_archivo(archivo):
+    Lista_Empleados = []
+    lineas = archivo.split(",")
+    for linea in lineas:
+        datos = linea.split(",")
+        nombre = datos[0]
+        correo = datos[1]
+        edad = int(datos[2])
+        departamento = datos[3]
+        salario = float(datos[4])
+        empleado = Empleado(nombre,correo,edad,departamento,salario) 
+        Lista_Empleados.append(empleado)
+
 
 class Empleado:
     def __init__(self,nombre,correo,edad,departamento,salario):
@@ -13,17 +36,9 @@ class Empleado:
         self.departamento = departamento
         self.salario = salario
 
-Lista_Empleados = []
 
-for linea in lineas:
-    datos = linea.split(",")
-    nombre = datos[0]
-    correo = datos[1]
-    edad = int(datos[2])
-    departamento = datos[3]
-    salario = float(datos[4])
-    empleado = Empleado(nombre,correo,edad,departamento,salario) 
-    Lista_Empleados.append(empleado)
+
+
 
 for empleado in Lista_Empleados:
     print("Nombre: ", empleado.nombre)
